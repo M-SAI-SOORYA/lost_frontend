4b4f9d085391cb3859f164f5781ddff26a5c1d9c
Marri Sai Soorya <96968484+M-SAI-SOORYA@users.noreply.github.com>
2024-12-02 21:58:06 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Home from "./pages/Home";
 import Find from "./pages/Find"
 import Post from "./pages/Post";
 import Details from "./pages/Details";
+import Register from "./login/Register";
+import Login from "./login/Login";
 import "./App.css"
 
 function App() {
    return (
     <Routes>
-      <Route path='/' element={<Home />}  />
+      <Route path="/" element ={<Register/>} />
+      <Route path="/register" element ={<Register/>} />
+      <Route path="/login" element ={<Login/>} />
+      <Route path='/home' element={<Home />}  />
       <Route path='/find' element={<Find />}  />
       <Route path='/post' element={<Post />}  />
       <Route path='/find/details/:id' element={<Details />}  />
